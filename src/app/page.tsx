41e25da031bb92d7d44f199b02ab8e4218cf0df5
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-12-08 09:01:22 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,6 +113,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function Home() {
   return (
     <>
+      <PageLoader />
       <HeroSection />
       <AchievmentsSection />
       <TargetAuditorySection />
